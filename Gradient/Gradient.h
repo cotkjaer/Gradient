60cb8b8b7992a30fea4b6fabf4caad7b179a0a63
christian otkjær <christian.otkjaer@gmail.com>
2016-02-18 17:33:11 +0100

    Initial Commit

---
+++ Gradient/Gradient.h
@@ -0,0 +1,19 @@
+//
+//  Gradient.h
+//  Gradient
+//
+//  Created by Christian Otkjær on 18/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gradient.
+FOUNDATION_EXPORT double GradientVersionNumber;
+
+//! Project version string for Gradient.
+FOUNDATION_EXPORT const unsigned char GradientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gradient/PublicHeader.h>
+
+
